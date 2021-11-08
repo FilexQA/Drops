ddbac1cf6303d6de03ece535f6801e34e383729b
Filex Maina <filextester@gmail.com>
2021-11-08 06:50:43 +0000

    Updated Navigation with Comments

--- cypress/integration/MainTest.spec.js
+++ cypress/integration/MainTest.spec.js
@@ -2,22 +2,16 @@
 describe("Navigate Site", () => {
     it("Open URL", () => {
         cy.visit('https://app.languagedrops.com') // visit Url
-    
   
-     cy.get('[data-testid=ENGB] > .r-1awozwy > .css-901oao')
+     cy.get('[data-testid=ENGB] > .r-1awozwy > .css-901oao')// Select English(UK) from selection of Languages
         .click()
   
-     cy.get('#root > div > div > div > div > div.css-1dbjc4n.r-1p0dtai.r-13awgt0.r-a9hzal.r-1d2f490.r-12vffkv.r-u8s1d.r-zchlnj.r-ipm5af.r-1jjucrs > div > div > div:nth-child(2) > div > div.css-1dbjc4n.r-u8s1d > div > img')
+      cy.get('[data-testid=CloseButton]')   // Close Helping Hand page
         .click()
 
-     cy.get('#root > div > div > div > div > div.css-1dbjc4n.r-a9hzal.r-1d2f490.r-1udh08x.r-ipm5af.r-1jjucrs > div:nth-child(2) > div > div > div > div > div.css-1dbjc4n.r-u8s1d > div > img')
-        .click()
-
-     
-     cy.get('[data-testid=FeatureIntroductionAction]')
-        .click()
+      cy.get('#root > div > div > div > div > div.css-1dbjc4n.r-a9hzal.r-1d2f490.r-1udh08x.r-ipm5af.r-1jjucrs > div:nth-child(2) > div > div > div > div > div.css-1dbjc4n.r-u8s1d > div > img')
+      .click('topLeft')   // close Drops Premium Subscription Page
           
-
 
         
     });
